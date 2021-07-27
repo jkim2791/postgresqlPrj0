42943b9a6622143ad9df0094ec4af3ebd2f810a3
skql951123@gmail.com <jkim2791@berkeley.edu>
2021-07-27 15:34:42 +0900

    code fix

--- etl.py
+++ etl.py
@@ -6,6 +6,9 @@ from sql_queries import *
 
 
 def get_files(filepath):
+    '''
+    this function reads json files to insert data into tables
+    '''
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
@@ -16,17 +19,16 @@ def get_files(filepath):
 
 
 def process_song_file(cur, filepath):
+    '''
+    - Read json formated song file as dataframe and divide into two tables which are song_data and artist_data
+    - Insert data into tables
+    '''
     # open song file
-    song_path = 'data/song_data'
-    song_files = get_files(song_path)
-    filepath = song_files[0]
     df = pd.read_json(filepath, lines=True)
-
     # insert song record
     song_data = df[["song_id", "title", "artist_id", "year", "duration"]].values[0]
     song_data = list(song_data)
     cur.execute(song_table_insert, song_data)
-    
     # insert artist record
     artist_data = df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0]
     artist_data = list(artist_data)
@@ -34,12 +36,12 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    '''
+    - Read json formated log file as dataframe and divide into two tables which are ts_df and time_df
+    - Insert data into tables
+    '''
     # open log file
-    log_path = 'data/log_data'
-    log_files = get_files(log_path)
-    filepath = log_files[0]
     log_df = pd.read_json(filepath, lines=True)
-    
     # filter by NextSong action
     log_df = log_df[log_df['page']=="NextSong"]
     ts_df = log_df[['ts']]
@@ -67,7 +69,6 @@ def process_log_file(cur, filepath):
     # insert songplay records
     log_df = log_df.drop_duplicates().dropna()
     for index, row in log_df.iterrows():
-        
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
@@ -85,6 +86,9 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+    '''
+    test for inserting data from filepath
+    '''
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -104,6 +108,11 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    '''
+    - connect to the database
+    - process all data
+    - close the database connection
+    '''
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
